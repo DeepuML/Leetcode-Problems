cb8636682c40f1f8c84fcd9f7b17a803f250c32f
Deependra Gangwar <116296894+DeepuML@users.noreply.github.com>
2026-07-25 23:43:33 +0530

    Time: 3 ms (27.60%) | Memory: 10 MB (60.43%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
-    void backtrack(int index, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans) {
-        ans.push_back(ds); 
-        for (int i = index; i < nums.size(); i++) {
-            ds.push_back(nums[i]);
-            backtrack(i + 1, nums, ds, ans); 
-            ds.pop_back(); 
-        }
+  void solve(int index , vector<int> &ds, vector<vector<int>> &ans, vector<int> &nums){
+    if(index==nums.size()){
+        ans.push_back(ds);
+        return ;
     }
+    // pick
+    ds.push_back(nums[index]);
+    solve(index+1,ds,ans,nums);
+    // remove
+    ds.pop_back();
+    // not pick
+    solve(index+1,ds,ans,nums);
+  }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
         vector<int> ds;
-        backtrack(0, nums, ds, ans);
-        return ans;
+        vector<vector<int>> ans;
+        solve(0,ds,ans,nums);
+        return ans; 
     }
 };
